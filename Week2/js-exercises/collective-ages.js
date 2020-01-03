add63265e733fdc717b994ae8d6fb1923c95a32d
Abuzer ALACA <aalaca.33@gmail.com>
2020-01-04 00:07:18 +0100

    Added collective-ages.js file

---
+++ Week2/js-exercises/collective-ages.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const hackYourFutureMembers = [
+  { name: 'Wouter', age: 33 },
+  { name: 'Federico', age: 32 },
+  { name: 'Noer', age: 27 },
+  { name: 'Tjebbe', age: 22 },
+];
+
+
+const collectiveAge = (array) => {
+  const agesOfMembers = hackYourFutureMembers.map(member => member.age);
+  const totalAge = agesOfMembers.reduce((total, member) => total += member);
+  return `The collective age of the HYF team is: [${totalAge}]`;
+}
+
+console.log(collectiveAge(hackYourFutureMembers));
